dfbff6b252c6d6f8660be5053f5b82cfd1263c60
shin0729 <shin.yamaguchi0801@gmail.com>
2023-12-22 09:07:50 +0900

    refactor: update variable names for clarity

--- src/extract_xml.py
+++ src/extract_xml.py
@@ -1,10 +1,9 @@
 from pathlib import Path
-def convert_to_zip(txt) -> Path:
-    new_xml = txt.with_suffix(".zip")
-    txt.rename(new_xml)
+def convert_to_zip(path_docx) -> Path:
+    new_xml = path_docx.with_suffix(".zip")
+    path_docx.rename(new_xml)
 
-def extract_xml(txt):
-    base_folder = txt.parent
+def extract_xml(path_zip):
+    base_folder = path_zip.parent
     for p in base_folder.glob("**/document.xml"):
         return p
-
