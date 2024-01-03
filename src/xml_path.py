9a2e81ae812ff60f27cac2b6a0d5995160bf0f6c
shin0729 <shin.yamaguchi0801@gmail.com>
2024-01-03 14:35:38 +0900

    Revert "Revert "feat: add main method with potential for future modifications""
    This reverts commit 5d24bf2d7231225a79c0e835cf959ee31d69222b.

--- src/xml_path.py
+++ src/xml_path.py
@@ -2,23 +2,36 @@ from pathlib import Path
 import re
 def extract_target_row(file_path):
     with open(file_path) as f:
-        contents_list = f.readlines()
+        contents_list = f.read()
         target_row = contents_list[1]
-        print("target_row: " + target_row)
         return target_row
 
 def split_into_tag_and_content(target_row):
     target_list = re.split(">|<", target_row)
-    print("result: " + target_list)
     return target_list
 
-def get_tagged_contents(target_list):
-    tag_open = False
-    for item in target_list:
-        if item == "w:t":
-            tag_open = True
-        elif item == "/w:t":
-            tag_open = False
-        elif tag_open:
-            yield item
+def get_bracket_contents(target_list):
+    depth = 0
+    f = 0
+    for i, c in enumerate(target_list):
+        if c == "w:t":
+            depth += 1
+            if depth == 1:
+                f = i + 1
+        
 
+
+
+
+# from pathlib import Path
+# import re
+
+# def extract_contents(file_path):
+#     pattern = re.compile(r"<w:t>(.*?)</w:t>")
+#     contents_list = []
+
+#     with open(file_path, mode="r", encoding="utf-8") as f:
+#         text = f.read()
+#         contents_list = [match.group(1) for match in pattern.finditer(text)]
+
+#     return contents_list
