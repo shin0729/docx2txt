a4c9fbd778583027999b3ee6c53af82df6ec6891
shin0729 <shin.yamaguchi0801@gmail.com>
2024-01-03 14:36:32 +0900

    feat: add get_tagged_contents method

--- src/xml_path.py
+++ src/xml_path.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import re
 def extract_target_row(file_path):
     with open(file_path) as f:
-        contents_list = f.read()
+        contents_list = f.readlines()
         target_row = contents_list[1]
         return target_row
 
@@ -10,15 +10,15 @@ def split_into_tag_and_content(target_row):
     target_list = re.split(">|<", target_row)
     return target_list
 
-def get_bracket_contents(target_list):
-    depth = 0
-    f = 0
-    for i, c in enumerate(target_list):
-        if c == "w:t":
-            depth += 1
-            if depth == 1:
-                f = i + 1
-        
+def get_tagged_contents(target_list):
+    tag_open = False
+    for item in target_list:
+        if item == "w:t":
+            tag_open = True
+        elif item == "/w:t":
+            tag_open = False
+        elif tag_open:
+            yield item
 
 
 
